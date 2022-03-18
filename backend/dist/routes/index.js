f8c81e84e7dd096cc935272fe4e30d3255c2f1c2
VietAnh <vak63.uet.vnu@gmail.com>
2022-03-18 15:47:15 +0700

    delete unused folder

--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const authRouter = require('./auth.route');
-const pollRouter = require('./poll.route');
-function route(app) {
-    app.use('/poll', pollRouter);
-    app.use('/', authRouter);
-}
-module.exports = route;
